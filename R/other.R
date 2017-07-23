565203bae3e61a6bc5fcb06f73f69492af93e903
eduardohet <eduardohet@gmail.com>
2017-07-23 13:24:06 -0300

    Delete other.R

--- R/other.R
+++
@@ -1,7 +0,0 @@
-vals.text <- function(x, dec="default", digits=3){
-  res <- round(x, digits)
-  if(dec != "default") gsub(".", dec, res, fixed=T) else res
-}
-tidy.list <- function(x, pattern="\\1, e"){
-  sub("(.*),", pattern, paste(x, collapse = ", "))
-}
